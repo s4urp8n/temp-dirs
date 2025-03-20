359c29c887134ee0637c1c798020f211dbf28bca
tarasov <s4urp8n@yandex.ru>
2025-03-20 17:46:47 +0300

    + coverage

--- test.php
+++ test.php
@@ -4,6 +4,6 @@ require __DIR__ . '/vendor/autoload.php';
 ob_implicit_flush(true);
 ini_set('implicit_flush', 1);
 
-echo passthru('.\vendor\bin\phpunit --bootstrap="tests/bootstrap.php"');
+echo passthru('.\vendor\bin\phpunit --bootstrap="tests/bootstrap.php" --coverage-html coverage/html');
 
 
